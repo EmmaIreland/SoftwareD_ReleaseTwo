6dbf75256df09d0def56e6c4a469279e97800e3e
mcgat003 <mcgat003@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-24 22:47:05 +0000

    Figured out how to do unit tests for TeamController so we made one and got it to pass. Now to make many more tests.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseTwo/TeamQwertyuiop@9032 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/unit/surveyor/TeamControllerTests.groovy
+++ test/unit/surveyor/TeamControllerTests.groovy
@@ -7,11 +7,16 @@ class TeamControllerTests extends ControllerUnitTestCase {
     Course softwareDesign = new Course(abbreviation: 'CSCI 3601', name: 'Software Design', term: 'Spring', year: '2012', owner: instructor)
     User ian = new User(name: 'Ian M', email: 'ian@ian.com')
     User emma = new User(name: 'Emma I', email: 'emma@emma.com')
-    Project releaseOne = new Project(name: 'Release One', description: '', course: softwareDesign)
+    Project releaseOne = new Project(name: 'Release One', description: '', course: softwareDesign, teams: [])
     
     protected void setUp() {
         super.setUp()
         mockForConstraintsTests(Team)
+		mockDomain User, [instructor, ian, emma]
+		mockDomain Course, [softwareDesign]
+		mockDomain Team, []
+		mockDomain GroupAssignment, []
+		mockDomain Project, [releaseOne]
     }
 
     protected void tearDown() {
@@ -19,10 +24,19 @@ class TeamControllerTests extends ControllerUnitTestCase {
     }
 
     void testGroupCreation() {
-/*        mockDomain(Project)
-        releaseOne.params.groupNumber = 2
-        releaseOne.createAndSaveMany()
-        assertEquals 0, 'Group 0'.id
-        */
+		
+        
+		def controller = new TeamController()
+		controller.params.groupNumber = '2'
+		controller.params.projectInstance = releaseOne
+		
+		controller.createAndSaveMany()
+		
+		assertEquals "project", controller.redirectArgs.controller
+		assertEquals "show", controller.redirectArgs.action
+		
+		
+    
+        
     }
 }
