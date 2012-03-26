25d6f6785031f56ed1199d2498693fff7820c3f9
irela065 <irela065@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-26 00:16:03 +0000

    Wrote test for SurveyAssignmentTest
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseTwo/TeamQwertyuiop@9048 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/unit/surveyor/SurveyAssignmentTests.groovy
+++ test/unit/surveyor/SurveyAssignmentTests.groovy
@@ -3,15 +3,24 @@ package surveyor
 import grails.test.*
 
 class SurveyAssignmentTests extends GrailsUnitTestCase {
+    
+    User nic = new User(name: 'Nic McPhee', email: 'mcphee@gmail.com')
+    User sarah = new User(name: 'Sarah Buchanan', email: 'buchanan@gmail.com')
+
+    Survey survey = new Survey(owner: nic)
+    
     protected void setUp() {
         super.setUp()
+        mockForConstraintsTests(SurveyAssignment)
+        mockDomain(SurveyAssignment, [])
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testValidation() {
+        SurveyAssignment surveyAssignment = new SurveyAssignment(survey: survey, student: sarah)
+        assertTrue surveyAssignment.validate()
     }
 }
