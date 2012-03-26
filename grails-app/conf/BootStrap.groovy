4bbb313b4bca4674b949413e1b77904c4cb4935e
schil227 <schil227@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-26 01:18:03 +0000

    This one isn't broken
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseTwo/TeamQwertyuiop@9053 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,28 +9,33 @@ class BootStrap {
     def init = { servletContext ->
         if (noData()) {
             
-			User[] students = [
-			User nic = new User(name: 'Nic McPhee', email: 'mcphee@gmail.com'),
-            User sarah = new User(name: 'Sarah Buchanan', email: 'buchanan@gmail.com'),
-            User simon = new User(name: 'Simon Tillier', email: 'tillier@gmail.com'),
-            User phou = new User(name: 'Phou Lee', email: 'phoulee@gmail.com'),
-            User chris = new User(name: 'Chris Aga', email: 'chrisaga@gmail.com'),
-            User kevin = new User(name: 'Kevin Viratyosin', email: 'kevinviratyosin@gmail.com'),
-            User josh = new User(name: 'Josh Johnson', email: 'joshjohnson@gmail.com'),
-            User matthew = new User(name: 'Matthew Perrault', email: 'mattperrault@gmail.com'),
-            User emma = new User(name: 'Emma Ireland', email: 'emmaireland@gmail.com'),
-            User christopher = new User(name: 'Christopher Thomas', email: 'christhomas@gmail.com'),
-            User ian = new User(name: 'Ian McGathy', email: 'ianmcgathy@gmail.com'),
-            User steve = new User(name: 'Steve Jungst', email: 'stevejungst@gmail.com'),
-            User matt = new User(name: 'Matt Cotter', email: 'mattcotter@gmail.com'),
-            User annabel = new User(name: 'Annabel Lee', email: 'annabellee@poe.org'),
-            User bill = new User(name: 'Bill Gates', email: 'gates@microsoft.com'),
-            User albert = new User(name: 'Albert Einstein', email: 'einstein@gmail.com')]
-            
+			
+			User nic = new User(name: 'Nic McPhee', email: 'mcphee@gmail.com')
+            User sarah = new User(name: 'Sarah Buchanan', email: 'buchanan@gmail.com')
+            User simon = new User(name: 'Simon Tillier', email: 'tillier@gmail.com')
+            User phou = new User(name: 'Phou Lee', email: 'phoulee@gmail.com')
+            User chris = new User(name: 'Chris Aga', email: 'chrisaga@gmail.com')
+            User kevin = new User(name: 'Kevin Viratyosin', email: 'kevinviratyosin@gmail.com')
+            User josh = new User(name: 'Josh Johnson', email: 'joshjohnson@gmail.com')
+            User matthew = new User(name: 'Matthew Perrault', email: 'mattperrault@gmail.com')
+            User emma = new User(name: 'Emma Ireland', email: 'emmaireland@gmail.com')
+            User christopher = new User(name: 'Christopher Thomas', email: 'christhomas@gmail.com')
+            User ian = new User(name: 'Ian McGathy', email: 'ianmcgathy@gmail.com')
+            User steve = new User(name: 'Steve Jungst', email: 'stevejungst@gmail.com')
+            User matt = new User(name: 'Matt Cotter', email: 'mattcotter@gmail.com')
+            User annabel = new User(name: 'Annabel Lee', email: 'annabellee@poe.org')
+            User bill = new User(name: 'Bill Gates', email: 'gates@microsoft.com')
+            User albert = new User(name: 'Albert Einstein', email: 'einstein@gmail.com')
+            User[] students = [nic, sarah, simon, phou, chris, kevin, josh, matthew, emma, christopher, ian, steve, matt, annabel, bill, albert]
+			for(int i = 0; i < students.length; i++){
+				students[i].save(failOnError:true)
+			}
 			
             Course softwareDesign = new Course(abbreviation:'CSCI 3601', name:'Software Design', term:'Spring',year:'2012', owner: nic).save(failOnError:true)
             Course musicTheory = new Course(abbreviation:'MUS 1001', name:'Music Theory', term:'Fall',year:'2013', owner: simon).save(failOnError:true)
             Course frenchI = new Course(abbreviation:'FREN 1001', name:'French I', term:'May',year:'2014', owner: sarah).save(failOnError:true)
+			
+			
 
             new Enrollment(course: softwareDesign, student: kevin).save(failOnError:true)
             new Enrollment(course: softwareDesign, student: phou).save(failOnError:true)
