a00a1b8d877c7d1077ea2016ca34551ac568cc96
irela065 <irela065@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-27 23:02:16 +0000

    Added randomize boolean
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseTwo/TeamQwertyuiop@9088 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/domain/surveyor/Team.groovy
+++ grails-app/domain/surveyor/Team.groovy
@@ -6,6 +6,8 @@ class Team {
     
     static belongsTo = [project: Project]
     static hasMany = [groupAssignments: GroupAssignment]
+    
+    boolean randomize = false
 
     static constraints = {
         name blank: false
