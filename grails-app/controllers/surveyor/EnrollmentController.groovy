91d8770bd1da33de573d07580c610beb9aa06dcd
schil227 <schil227@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-03-29 02:23:50 +0000

    changed AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALL the strings in the controllers to a variable so sonar would shut up
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseTwo/TeamQwertyuiop@9132 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/controllers/surveyor/EnrollmentController.groovy
+++ grails-app/controllers/surveyor/EnrollmentController.groovy
@@ -2,10 +2,17 @@ package surveyor
 
 class EnrollmentController {
 
-    static allowedMethods = [save: 'POST', update: 'POST', delete: 'POST']
+	static def post = 'POST'
+	def listString = 'list'
+	def editString = 'edit'
+	def createString = 'create'
+	def showString = 'show'
+	def defaultNotFoundMessage = 'default.not.found.message'
+	
+    static allowedMethods = [save: post, update: post, delete: post]
 
     def index = {
-        redirect(action: 'list', params: params)
+        redirect(action: listString, params: params)
     }
 
     def list = {
@@ -23,18 +30,18 @@ class EnrollmentController {
         def enrollmentInstance = new Enrollment(params)
         if (enrollmentInstance.save(flush: true)) {
             flash.message = makeMessage('default.created.message', params.id)
-            redirect(action: 'show', id: enrollmentInstance.id)
+            redirect(action: showString, id: enrollmentInstance.id)
         }
         else {
-            render(view: 'create', model: [enrollmentInstance: enrollmentInstance])
+            render(view: createString, model: [enrollmentInstance: enrollmentInstance])
         }
     }
 
     def show = {
         def enrollmentInstance = Enrollment.get(params.id)
         if (!enrollmentInstance) {
-            flash.message = makeMessage('default.not.found.message', params.id)
-            redirect(action: 'list')
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
         }
         else {
             [enrollmentInstance: enrollmentInstance]
@@ -44,8 +51,8 @@ class EnrollmentController {
     def edit = {
         def enrollmentInstance = Enrollment.get(params.id)
         if (!enrollmentInstance) {
-            flash.message = makeMessage('default.not.found.message', params.id)
-            redirect(action: 'list')
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
         }
         else {
             return [enrollmentInstance: enrollmentInstance]
@@ -62,22 +69,22 @@ class EnrollmentController {
                     enrollmentInstance.errors.rejectValue('version', 'default.optimistic.locking.failure', 
 						[message(code: 'enrollment.label', default: 'Enrollment')] as Object[], 
 						'Another user has updated this Enrollment while you were editing')
-                    render(view: 'edit', model: [enrollmentInstance: enrollmentInstance])
+                    render(view: editString, model: [enrollmentInstance: enrollmentInstance])
                     return
                 }
             }
             enrollmentInstance.properties = params
             if (!enrollmentInstance.hasErrors() && enrollmentInstance.save(flush: true)) {
             flash.message = makeMessage('default.updated.message', params.id)
-                redirect(action: 'show', id: enrollmentInstance.id)
+                redirect(action: showString, id: enrollmentInstance.id)
             }
             else {
-                render(view: 'edit', model: [enrollmentInstance: enrollmentInstance])
+                render(view: editString, model: [enrollmentInstance: enrollmentInstance])
             }
         }
         else {
-            flash.message = makeMessage('default.not.found.message', params.id)
-            redirect(action: 'list')
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
         }
     }
 
@@ -87,16 +94,16 @@ class EnrollmentController {
             try {
                 enrollmentInstance.delete(flush: true)
 				flash.message = makeMessage('default.deleted.message', params.id)
-                redirect(action: 'list')
+                redirect(action: listString)
             }
             catch (org.springframework.dao.DataIntegrityViolationException e) {
 				flash.message = makeMessage('default.not.deleted.message', params.id)
-                redirect(action: 'show', id: params.id)
+                redirect(action: showString, id: params.id)
             }
         }
         else {
-            flash.message = makeMessage('default.not.found.message', params.id)
-            redirect(action: 'list')
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
         }
     }
 	
